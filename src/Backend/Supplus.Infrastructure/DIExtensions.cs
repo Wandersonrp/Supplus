e3c0407824bfcacd3c4b322f383722d7131a7062
Wanderson Rodrigues <91907486+Wandersonrp@users.noreply.github.com>
2026-01-24 14:16:27 -0300

    adiciona configuração de migrations com FluentMigrator (#1)

---
+++ src/Backend/Supplus.Infrastructure/DIExtensions.cs
@@ -0,0 +1,43 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Supplus.Infrastructure.Data.Context;
+using System.Reflection;
+
+namespace Supplus.Infrastructure;
+
+public static class DIExtensions
+{
+    public static void AdicionaInrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        ConfiguraDbContext(services, configuration);
+        ConfiguraFluentMigrator(services, configuration);
+    }
+    
+    private static void ConfiguraDbContext(IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("PostgreSql") ??
+            throw new Exception("Adicione a string de conexão 'PostgreSql'");
+
+        services.AddDbContext<SupplusDbContext>(opt =>
+        {
+            opt.UseNpgsql(connectionString);
+        });
+    }
+
+    private static void ConfiguraFluentMigrator(IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("PostgreSql") ??
+            throw new Exception("Adicione a string de conexão 'PostgreSql'");
+
+        services.AddFluentMigratorCore().ConfigureRunner(opt =>
+        {
+            opt.AddPostgres()
+                .WithGlobalConnectionString(connectionString)
+                .ScanIn(Assembly.Load("Supplus.Infrastructure"))
+                .For
+                .All();
+        });
+    }
+}
---
+++ src/Backend/Supplus.Infrastructure/Data/Context/SupplusDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Supplus.Infrastructure.Data.Context;
+
+public class SupplusDbContext : DbContext
+{
+    public SupplusDbContext(DbContextOptions<SupplusDbContext> options) : base(options)
+    {
+    }
+}
