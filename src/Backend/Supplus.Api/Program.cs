d169638d3958f1f9f25b63c738f4ed4b6d119202
Wandersonrp <wandersonrp.49@hotmail.com>
2026-01-24 14:13:58 -0300

    adiciona configuração de migrations com FluentMigrator

--- src/Backend/Supplus.Api/Program.cs
+++ src/Backend/Supplus.Api/Program.cs
@@ -1,10 +1,16 @@
+using Supplus.Infrastructure;
+using Supplus.Infrastructure.Data.Migrations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Adiciona DI do projeto de Infraestrutura
+builder.Services.AdicionaInrastructure(builder.Configuration);
 
 var app = builder.Build();
 
@@ -20,4 +26,12 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+AplicarMigration();
+
 app.Run();
+
+void AplicarMigration()
+{
+    using var scope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
+    ControleMigration.AplicarMigration(builder.Configuration.GetConnectionString("PostgreSql"), scope.ServiceProvider);
+}
---
+++ src/Backend/Supplus.Infrastructure/Data/Migrations/ControleMigration.cs
@@ -0,0 +1,65 @@
+﻿using Dapper;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+
+namespace Supplus.Infrastructure.Data.Migrations;
+
+public static class ControleMigration
+{
+    /// <summary>
+    /// Cria o banco de dados, caso não exista, e aplica as migrations pendentes utilizando o provedor de serviços.
+    /// </summary>
+    /// <param name="connectionString">
+    /// String de conexão utilizada para verificar e criar o banco de dados.
+    /// </param>
+    /// <param name="serviceProvider">
+    /// Provedor de serviços usado para executar as migrations configuradas.
+    /// </param>
+    public static void AplicarMigration(string connectionString, IServiceProvider serviceProvider)
+    {
+        CriarBanco(connectionString);
+        // AplicarMigration(serviceProvider);
+    }
+
+    /// <summary>
+    /// Aplica as migrations pendentes no banco de dados utilizando o serviço de migração configurado.
+    /// </summary>
+    /// <param name="serviceProvider">
+    /// Provedor de serviços usado para obter a instância do <see cref="IMigrationRunner"/>.
+    /// </param>
+    private static void AplicarMigration(IServiceProvider serviceProvider)
+    {
+        var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+        runner.ListMigrations();
+
+        if (runner.HasMigrationsToApplyUp())
+            runner.MigrateUp();
+    }
+
+    /// <summary>
+    /// Cria um banco de dados PostgreSQL caso ele não exista, utilizando a string de conexão fornecida.
+    /// </summary>
+    /// <param name="connectionString">
+    /// String de conexão para o servidor PostgreSQL.
+    /// </param>
+    private static void CriarBanco(string connectionString)
+    {
+        var connectionStringBuilder = new NpgsqlConnectionStringBuilder(connectionString);
+
+        var nomeDatabase = connectionStringBuilder.Database;
+
+        connectionStringBuilder.Remove("Database");
+
+        using var connection = new NpgsqlConnection(connectionStringBuilder.ConnectionString);
+
+        var parametros = new DynamicParameters();
+        parametros.Add("name", nomeDatabase);
+
+        var existe = connection.QueryFirstOrDefault<bool>("SELECT 1 FROM pg_database WHERE datname = @name;", parametros);
+
+        if (!existe)
+            connection.Execute($"CREATE DATABASE {nomeDatabase}");
+    }
+}
