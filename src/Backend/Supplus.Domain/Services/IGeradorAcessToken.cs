8a863c917889c965b9e50d232ff89fee0d579932
Wandersonrp <wandersonrp.49@hotmail.com>
2026-01-25 12:43:56 -0300

    adiciona gerador de access token jwt

---
+++ src/Backend/Supplus.Domain/Services/IGeradorAcessToken.cs
@@ -0,0 +1,8 @@
+﻿using Supplus.Domain.Enums;
+
+namespace Supplus.Domain.Services;
+
+public interface IGeradorAccessToken
+{
+    string Gerar(Guid identificador, Role role);
+}
---
+++ src/Backend/Supplus.Infrastructure/Services/Seguranca/Tokens/Jwt/GeradorTokenJwt.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Supplus.Domain.Enums;
+using Supplus.Domain.Services;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Supplus.Infrastructure.Services.Seguranca.Tokens.Jwt;
+
+public class GeradorTokenJwt : JwtTokenHandler, IGeradorAccessToken
+{
+    private readonly string _chaveAssinatura;
+    private readonly uint _expiracaoEmMinutos;
+
+    public GeradorTokenJwt(string chaveAssinatura, uint expiracaoEmMinutos)
+    {
+        _chaveAssinatura = chaveAssinatura;
+        _expiracaoEmMinutos = expiracaoEmMinutos;
+    }
+
+    public string Gerar(Guid identificador, Role role)
+    {
+        var claims = new List<Claim>()
+        {
+            new Claim(ClaimTypes.Sid, identificador.ToString()),
+            new Claim(ClaimTypes.Role, role.ToString())
+        };
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {            
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddMinutes(_expiracaoEmMinutos),
+            SigningCredentials = new SigningCredentials(ChaveSeguranca(_chaveAssinatura), SecurityAlgorithms.HmacSha256Signature),
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
