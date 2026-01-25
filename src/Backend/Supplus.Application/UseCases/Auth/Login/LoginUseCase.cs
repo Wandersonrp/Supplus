dec87ea3e0282db0364d33382b617fd79eec61a6
Wandersonrp <wandersonrp.49@hotmail.com>
2026-01-25 11:20:09 -0300

    refatora use case de login, adicionando validação, chamada de repositórios e serviços

--- src/Backend/Supplus.Application/UseCases/Auth/Login/LoginUseCase.cs
+++ src/Backend/Supplus.Application/UseCases/Auth/Login/LoginUseCase.cs
@@ -1,22 +1,45 @@
-﻿using Supplus.Comunicacao.Requests.Auth;
+﻿using Microsoft.AspNetCore.Identity;
+using Supplus.Comunicacao.Requests.Auth;
 using Supplus.Comunicacao.Responses.Auth;
+using Supplus.Comunicacao.Validators.Auth.Login;
+using Supplus.Domain.Entities;
+using Supplus.Domain.Repositories;
+using Supplus.Exceptions;
 
 namespace Supplus.Application.UseCases.Auth.Login;
 
-public class LoginUseCase
+public class LoginUseCase : BaseUseCase<RequestLoginJson, LoginValidator>, ILoginUseCase
 {
-    public LoginUseCase()
+    private readonly IUsuarioRepository _usuarioRepository;
+    private readonly IPasswordHasher<Usuario> _passwordHasher;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public LoginUseCase(
+        IUsuarioRepository usuarioRepository, 
+        IPasswordHasher<Usuario> passwordHasher, 
+        IUnitOfWork unitOfWork)
     {
+        _usuarioRepository = usuarioRepository;
+        _passwordHasher = passwordHasher;
+        _unitOfWork = unitOfWork;
     }
      
-    public ResponseLoginJson Executar(RequestLoginJson request)
-    {
-        // Lógica de autenticação simulada para o exemplo
-        if (string.IsNullOrEmpty(request.Email) || string.IsNullOrEmpty(request.Senha))
-            throw new ArgumentException("Email e senha são obrigatórios.");
+    public async Task<ResultadoPersonalizado<ResponseLoginJson>> Executar(RequestLoginJson request)
+    {        
+        var resultado = Validar(request);
 
-        if (request.Email != "admin@admin.com" || request.Senha != "12345678")
-            throw new ArgumentException("E-mail ou senha incorretos.");
+        if(resultado != ErroPadronizado.Nenhum)
+            return ResultadoPersonalizado<ResponseLoginJson>.Falha(resultado);        
+
+        var usuario = await _usuarioRepository.ObterPorEmailAsync(request.Email!);
+
+        if(usuario is null)
+            return ResultadoPersonalizado<ResponseLoginJson>.Falha(ErroPadronizado.CredencialInvalidaErro());
+
+        var resultadoSenha = _passwordHasher.VerifyHashedPassword(usuario, usuario.ObterSenha(), request.Senha!);
+
+        if(resultadoSenha == PasswordVerificationResult.Failed)
+            return ResultadoPersonalizado<ResponseLoginJson>.Falha(ErroPadronizado.CredencialInvalidaErro());                
 
         // Simula a geração de tokens
         var accessToken = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
@@ -24,6 +47,8 @@ public class LoginUseCase
 
         var token = new ResponseTokenJson(accessToken, refreshToken);
 
-        return new ResponseLoginJson(token);
+        await _unitOfWork.CommitAsync();
+
+        return ResultadoPersonalizado<ResponseLoginJson>.Sucesso(new ResponseLoginJson(token));
     }
 }
