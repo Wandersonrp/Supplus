857eb8c30a98fe3a11a5a021e54cd2a6fee3bd82
Wandersonrp <wandersonrp.49@hotmail.com>
2026-01-25 13:54:55 -0300

    adiciona migração para criar tabela de Usuários

---
+++ src/Backend/Supplus.Infrastructure/Data/Migrations/Versoes/Versao0000001.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace Supplus.Infrastructure.Data.Migrations.Versoes;
+
+[Migration(VersoesMigration.CRIAR_TABELA_USUARIO, "Cria tabela de Usuario")]
+public class Versao0000001 : VersaoBase
+{
+    public override void Up()
+    {
+        CriarTabela(tabela: "Usuarios", nomeId: "IdUsuario")
+            .WithColumn("Nome").AsString(50).NotNullable()
+            .WithColumn("Sobrenome").AsString(50).NotNullable()
+            .WithColumn("Email").AsString(150).NotNullable().Indexed()
+            .WithColumn("NomeCompleto").AsString(101).NotNullable()
+            .WithColumn("Senha").AsString().NotNullable()
+            .WithColumn("Role").AsInt32().NotNullable();
+    }
+}
---
+++ src/Backend/Supplus.Infrastructure/Data/Migrations/Versoes/VersaoBase.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+using FluentMigrator.Builders.Create.Table;
+
+namespace Supplus.Infrastructure.Data.Migrations.Versoes;
+
+public abstract class VersaoBase : ForwardOnlyMigration
+{
+    protected ICreateTableColumnOptionOrWithColumnSyntax CriarTabela(string tabela, string nomeId)
+    {
+        return Create.Table(tabela)
+            .WithColumn(nomeId).AsInt64().PrimaryKey().Identity()
+            .WithColumn("IdExterno").AsGuid().NotNullable().Indexed()
+            .WithColumn("CriadoEm").AsDateTime().NotNullable()
+            .WithColumn("CriadoPor").AsInt64().Nullable()
+            .WithColumn("AtualizadoEm").AsDateTime().Nullable()
+            .WithColumn("DeletadoEm").AsDateTime().Nullable()
+            .WithColumn("DeletadoPor").AsInt64().Nullable()
+            .WithColumn("MotivoDelecao").AsString(300).Nullable()
+            .WithColumn("Status").AsInt32().NotNullable();
+    }
+}
