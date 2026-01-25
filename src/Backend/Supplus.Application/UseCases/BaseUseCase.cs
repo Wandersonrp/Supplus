e87a09cb207b9a3dbfbcc8e1097b86c86b2b5776
Wandersonrp <wandersonrp.49@hotmail.com>
2026-01-25 11:06:07 -0300

    adiciona interface ILoginUseCase e uma classe base para UseCases que contém um método genérico de validação de requests

---
+++ src/Backend/Supplus.Application/UseCases/BaseUseCase.cs
@@ -0,0 +1,21 @@
+﻿using Supplus.Comunicacao.Validators;
+using Supplus.Exceptions;
+
+namespace Supplus.Application.UseCases;
+
+public class BaseUseCase<TRequest, TValidator> where TValidator : BaseValidator<TRequest>, new()
+{
+    protected virtual ErroPadronizado Validar(TRequest request)
+    {
+        var validator = new TValidator();
+        var resultado = validator.Validate(request);
+
+        if (!resultado.IsValid)
+        {
+            var errors = resultado.Errors.Select(e => e.ErrorMessage).ToList();
+            return ErroPadronizado.ErroDeValidacaoMensagens(errors);
+        }
+
+        return ErroPadronizado.Nenhum;
+    }
+}
