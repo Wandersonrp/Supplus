3a969438b3d48ac9fd6f42dc590675953a1b109f
Wandersonrp <wandersonrp.49@hotmail.com>
2026-01-25 11:03:09 -0300

    adiciona propriedade Role na entidade usuário

--- src/Backend/Supplus.Domain/Entities/Usuario.cs
+++ src/Backend/Supplus.Domain/Entities/Usuario.cs
@@ -1,4 +1,6 @@
-﻿namespace Supplus.Domain.Entities;
+﻿using Supplus.Domain.Enums;
+
+namespace Supplus.Domain.Entities;
 
 public sealed class Usuario : EntidadeBase
 {
@@ -6,17 +8,19 @@ public sealed class Usuario : EntidadeBase
     public string Nome { get; private set; }
     public string Sobrenome { get; private set; }
     public string NomeCompleto { get; }
+    public Role Role { get; private set; }
 
     private string _senha;
     
-    public Usuario(string email, string nome, string sobrenome)
+    public Usuario(string email, string nome, string sobrenome, Role? role = null)
     {
         _senha = string.Empty;
         
         Email = email;
         Nome = nome;
         Sobrenome = sobrenome;
-        NomeCompleto = ObterNomeCompleto();
+        NomeCompleto = ObterNomeCompleto();        
+        Role = role ?? Role.UsuarioComum;
     }
 
     public void AtribuirSenha(string senha)
