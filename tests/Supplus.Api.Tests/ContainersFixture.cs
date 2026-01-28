aa05c3c52f1ed8cecfa6a9ed6c8ba8ad969d13b2
Wandersonrp <wandersonrp.49@hotmail.com>
2026-01-27 21:51:13 -0300

    adiciona teste de integração para a funcionalidade de login

---
+++ tests/Supplus.Api.Tests/ContainersFixture.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Supplus.Infrastructure.Data.Context;
+using Testcontainers.PostgreSql;
+
+namespace Supplus.Api.Tests;
+
+public class ContainersFixture : IAsyncLifetime
+{
+    public PostgreSqlContainer Container { get; set; }
+
+    public ContainersFixture()
+    {
+        Container = new PostgreSqlBuilder(image: "postgres:latest")
+            .Build();
+    }
+
+    public async Task InitializeAsync() => await Container.StartAsync();
+
+    public async Task DisposeAsync() => await Container.StopAsync();
+}
