91694e917ec5046bfe6da5e30577e5006d5d6804
Wandersonrp <wandersonrp.49@hotmail.com>
2026-01-27 22:00:50 -0300

    adiciona modelagem de algumas propriedades do Usuario

--- src/Backend/Supplus.Infrastructure/Data/Context/SupplusDbContext.cs
+++ src/Backend/Supplus.Infrastructure/Data/Context/SupplusDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Supplus.Domain.Entities;
+using Supplus.Domain.Enums;
 
 namespace Supplus.Infrastructure.Data.Context;
 
@@ -10,4 +11,20 @@ public class SupplusDbContext : DbContext
     }
 
     public DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(e =>
+        {
+            e.HasQueryFilter(u => u.Status == Status.Ativo);
+
+            e.Property(u => u.Id)
+                .HasColumnName("IdUsuario");
+
+            e.Property<string>("_senha")
+                .HasColumnName("Senha");
+        });
+            
+        base.OnModelCreating(modelBuilder);
+    }
 }
