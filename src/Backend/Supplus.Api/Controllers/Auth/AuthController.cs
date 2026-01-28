2cabd1efe11ae065a0acf8d6bf25be631416ddf4
Wandersonrp <wandersonrp.49@hotmail.com>
2026-01-27 21:38:20 -0300

    adiciona controller AuthController

---
+++ src/Backend/Supplus.Api/Controllers/Auth/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Supplus.Application.UseCases.Auth.Login;
+using Supplus.Comunicacao.Requests.Auth;
+using Supplus.Exceptions;
+
+namespace Supplus.Api.Controllers.Auth;
+
+public class AuthController : BaseController
+{
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginAsync([FromBody] RequestLoginJson request, [FromServices] ILoginUseCase useCase)
+    {
+        var resultado = await useCase.Executar(request);
+
+        if(resultado.Falhou)
+        {
+            return resultado.Erro.Codigo switch
+            {
+                nameof(CodigosErro.ErroDeValidacao) => BadRequest(resultado.Erro),
+                nameof(CodigosErro.CredencialInvalida) => Unauthorized(resultado.Erro),
+                _ => StatusCode(StatusCodes.Status500InternalServerError, "Erro desconhecido.")
+            };
+        }
+
+        return Ok(resultado.Valor);
+    }
+}
