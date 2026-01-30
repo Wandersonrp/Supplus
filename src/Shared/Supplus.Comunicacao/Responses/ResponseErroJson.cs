521996c6545f380603ca3cdc39ec82999d088e4b
Wandersonrp <wandersonrp.49@hotmail.com>
2026-01-30 13:48:06 -0300

    adiciona resposta de erro personalizada

---
+++ src/Shared/Supplus.Comunicacao/Responses/ResponseErroJson.cs
@@ -0,0 +1,16 @@
+﻿namespace Supplus.Comunicacao.Responses;
+
+public record ResponseErroJson
+{
+    public List<string> Mensagens { get; private set; }    
+
+    public ResponseErroJson(List<string> mensagens)
+    {
+        Mensagens = mensagens;
+    }
+
+    public ResponseErroJson(string mensagem)
+    {
+        Mensagens = [mensagem];
+    }
+}
