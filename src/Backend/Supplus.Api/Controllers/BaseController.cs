d100bcd0ec1b3928d7003a823d24559cc35fc12f
Wandersonrp <wandersonrp.49@hotmail.com>
2026-01-30 13:49:14 -0300

    adiciona método de resposta http de acordo com o código de erro

--- src/Backend/Supplus.Api/Controllers/BaseController.cs
+++ src/Backend/Supplus.Api/Controllers/BaseController.cs
@@ -1,9 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Supplus.Comunicacao.Responses;
+using Supplus.Exceptions;
 
 namespace Supplus.Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 public abstract class BaseController : ControllerBase
-{    
+{ 
+    protected IActionResult HandlerResponse<TResultado>(ResultadoPersonalizado<TResultado> resultado, Func<TResultado, IActionResult> objectResult)
+    {
+        if(resultado.Falhou)
+        {
+            return resultado.Erro.Codigo switch
+            {
+                nameof(CodigosErro.ErroDeValidacao) => BadRequest(new ResponseErroJson(resultado.Erro.Mensagens!)),
+                nameof(CodigosErro.NaoEncontrado) => NotFound(new ResponseErroJson(resultado.Erro.Mensagem!)),
+                nameof(CodigosErro.Conflito) => Conflict(new ResponseErroJson(resultado.Erro.Mensagem!)),
+                nameof(CodigosErro.NaoAutorizado) or 
+                    nameof(CodigosErro.CredencialInvalida) => Unauthorized(new ResponseErroJson(resultado.Erro.Mensagem!)),                
+                _ => throw new NotImplementedException("Código de erro não mapeado para resposta HTTP.")
+            };
+        }
+
+        return objectResult(resultado.Valor);
+    }
 }
