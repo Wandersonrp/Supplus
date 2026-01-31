9aba1e823a1e50189d7afb940561d6a73292faeb
Wandersonrp <wandersonrp.49@hotmail.com>
2026-01-31 15:57:29 -0300

    adiciona rota para geração do refresh token

--- src/Backend/Supplus.Api/Controllers/Auth/AuthController.cs
+++ src/Backend/Supplus.Api/Controllers/Auth/AuthController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Supplus.Application.UseCases.Auth.Login;
+using Supplus.Application.UseCases.Auth.RereshTokens;
 using Supplus.Comunicacao.Requests.Auth;
+using Supplus.Comunicacao.Requests.RefreshTokens;
+using Supplus.Domain;
 
 namespace Supplus.Api.Controllers.Auth;
 
@@ -13,5 +16,30 @@ public class AuthController : BaseController
 
         var resultado = await useCase.Executar(request, ip, userAgent);
         return HandlerResponse(resultado, Ok);                
+    }
+
+    [HttpPost("refresh-token")]
+    public async Task<IActionResult> GerarRefreshTokenAsync(
+        [FromServices] IGerarRefreshTokenUseCase useCase, 
+        HttpContext httpContext, 
+        CancellationToken token)
+    {
+        (string? ip, string userAgent) = ObterInformacoesDispositivo();
+
+        var request = new RequestNovoTokenJson(ObterRefreshToken(httpContext), ip ?? string.Empty, userAgent);
+        var resultado = await useCase.Executar(request, token);
+
+        if(resultado.Falhou)
+            return TratarFalha(resultado);
+
+        httpContext.Response.Cookies.Append("refresh_token", resultado.Valor.RefreshToken, new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.Strict,
+            Expires = DateTime.UtcNow.AddHours(RegrasConstants.EXPIRACAO_REFRESH_TOKEN_HORAS)
+        });
+
+        return Ok(resultado.Valor);
     }
 }
