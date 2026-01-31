01e7e1fc02c2f562ca11fddd22ef8c80b9170c4c
Wandersonrp <wandersonrp.49@hotmail.com>
2026-01-31 15:53:44 -0300

    separa método de tratar falha para retornar resposta corretamente e adiciona método para obter refresh token dos cookies

--- src/Backend/Supplus.Api/Controllers/BaseController.cs
+++ src/Backend/Supplus.Api/Controllers/BaseController.cs
@@ -11,19 +11,23 @@ public abstract class BaseController : ControllerBase
     protected IActionResult HandlerResponse<TResultado>(ResultadoPersonalizado<TResultado> resultado, Func<TResultado, IActionResult> objectResult)
     {
         if(resultado.Falhou)
-        {
-            return resultado.Erro.Codigo switch
-            {
-                nameof(CodigosErro.ErroDeValidacao) => BadRequest(new ResponseErroJson(resultado.Erro.Mensagens!)),
-                nameof(CodigosErro.NaoEncontrado) => NotFound(new ResponseErroJson(resultado.Erro.Mensagem!)),
-                nameof(CodigosErro.Conflito) => Conflict(new ResponseErroJson(resultado.Erro.Mensagem!)),
-                nameof(CodigosErro.NaoAutorizado) or 
-                    nameof(CodigosErro.CredencialInvalida) => Unauthorized(new ResponseErroJson(resultado.Erro.Mensagem!)),                
-                _ => throw new NotImplementedException("Código de erro não mapeado para resposta HTTP.")
-            };
-        }
+            return TratarFalha(resultado);
 
         return objectResult(resultado.Valor);
+    }
+
+    protected IActionResult TratarFalha<TResultado>(ResultadoPersonalizado<TResultado> resultado)
+    {
+        return resultado.Erro.Codigo switch
+        {
+            nameof(CodigosErro.ErroDeValidacao) => BadRequest(new ResponseErroJson(resultado.Erro.Mensagens!)),
+            nameof(CodigosErro.NaoEncontrado) => NotFound(new ResponseErroJson(resultado.Erro.Mensagem!)),
+            nameof(CodigosErro.Conflito) => Conflict(new ResponseErroJson(resultado.Erro.Mensagem!)),
+            nameof(CodigosErro.NaoAutorizado) or
+                nameof(CodigosErro.CredencialInvalida) => Unauthorized(resultado?.Erro?.Mensagem is not null ?
+                new ResponseErroJson(resultado.Erro.Mensagem!) : null),
+            _ => throw new NotImplementedException("Código de erro não mapeado para resposta HTTP.")
+        };
     }
 
     protected (string? ip, string userAgent) ObterInformacoesDispositivo()
@@ -31,5 +35,15 @@ public abstract class BaseController : ControllerBase
         var ip = HttpContext.Connection.RemoteIpAddress?.ToString();
         var userAgent = HttpContext.Request.Headers.UserAgent.ToString();        
         return (ip, userAgent);
+    }
+
+    protected string ObterRefreshToken(HttpContext httpContext)
+    {
+        var refreshToken = string.Empty;
+
+        if (httpContext.Request.Cookies.TryGetValue("refresh_token", out var resultado))
+            refreshToken = resultado;
+
+        return refreshToken;
     }
 }
