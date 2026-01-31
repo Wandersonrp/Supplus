6a926c6e01fc7d9ed421d566df99cfd5ea047a04
Wandersonrp <wandersonrp.49@hotmail.com>
2026-01-31 15:49:24 -0300

    adiciona request de geração de novo refresh token

---
+++ src/Shared/Supplus.Comunicacao/Requests/RefreshTokens/RequestNovoTokenJson.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Supplus.Comunicacao.Requests.RefreshTokens;
+
+public record RequestNovoTokenJson(
+    [property: JsonIgnore] string RefreshToken, 
+    [property: JsonIgnore] string Ip, 
+    [property: JsonIgnore] string DispositivoInfo);
