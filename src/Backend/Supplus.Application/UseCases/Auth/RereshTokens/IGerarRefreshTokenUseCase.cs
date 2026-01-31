9f33b53cf6fbeb8cf306338863ad99ff1f175246
Wandersonrp <wandersonrp.49@hotmail.com>
2026-01-31 15:50:24 -0300

    adiciona use case de gerar refresh token

---
+++ src/Backend/Supplus.Application/UseCases/Auth/RereshTokens/IGerarRefreshTokenUseCase.cs
@@ -0,0 +1,10 @@
+﻿using Supplus.Comunicacao.Requests.RefreshTokens;
+using Supplus.Comunicacao.Responses.Auth;
+using Supplus.Exceptions;
+
+namespace Supplus.Application.UseCases.Auth.RereshTokens;
+
+public interface IGerarRefreshTokenUseCase
+{
+    Task<ResultadoPersonalizado<ResponseTokenJson>> Executar(RequestNovoTokenJson request, CancellationToken token);
+}
