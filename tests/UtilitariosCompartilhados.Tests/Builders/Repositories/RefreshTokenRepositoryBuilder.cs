4cc9313e2950f6cdcf9d4932d26cdf388999ce03
Wandersonrp <wandersonrp.49@hotmail.com>
2026-01-31 15:37:35 -0300

    adiciona mock do repositório do refresh token

---
+++ tests/UtilitariosCompartilhados.Tests/Builders/Repositories/RefreshTokenRepositoryBuilder.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using Supplus.Domain.Entities;
+using Supplus.Domain.Repositories;
+
+namespace UtilitariosCompartilhados.Tests.Builders.Repositories;
+
+public class RefreshTokenRepositoryBuilder
+{
+    private readonly Mock<IRefreshTokenRepository> _mock;
+
+    public RefreshTokenRepositoryBuilder()
+    {
+        _mock = new Mock<IRefreshTokenRepository>();
+    }
+
+    public void ObterRefreshTokenPorTokenAsync(RefreshToken refreshToken)
+    {
+        _mock
+            .Setup(r => r.ObterRefreshTokenPorTokenAsync(refreshToken.Token, CancellationToken.None))
+            .ReturnsAsync(refreshToken);
+    }
+
+    public IRefreshTokenRepository Build() => _mock.Object;
+}
