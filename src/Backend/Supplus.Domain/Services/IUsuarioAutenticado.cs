50f669ae57e9fa42092be36594de5819eddfb079
Wandersonrp <wandersonrp.49@hotmail.com>
2026-02-07 09:26:37 -0300

    adiciona serviço para obter o usuário logado a partir do token enviado na requisição

---
+++ src/Backend/Supplus.Domain/Services/IUsuarioAutenticado.cs
@@ -0,0 +1,23 @@
+﻿using Supplus.Domain.Models;
+
+namespace Supplus.Domain.Services;
+
+/// <summary>
+/// Define o contrato para serviços que obtêm informações do usuário autenticado.
+/// </summary>
+/// <remarks>
+/// A interface abstrai a lógica de recuperação dos dados do usuário atualmente autenticado,
+/// permitindo que diferentes implementações forneçam essa informação conforme o contexto
+/// (por exemplo, via token JWT, sessão ou outro mecanismo de autenticação).
+/// </remarks>
+public interface IUsuarioAutenticado
+{
+    /// <summary>
+    /// Obtém de forma assíncrona os dados do usuário autenticado no sistema.
+    /// </summary>
+    /// <returns>
+    /// Uma tarefa que resulta em um objeto <see cref="UsuarioAutenticado"/> contendo
+    /// o identificador, e-mail e a role do usuário autenticado.
+    /// </returns>
+    Task<UsuarioAutenticado> ObterUsuarioAutenticadoAsync();
+}
---
+++ src/Backend/Supplus.Infrastructure/Services/UsuarioAutenticadoService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Supplus.Domain.Models;
+using Supplus.Domain.Services;
+using Supplus.Domain.Services.Tokens;
+using Supplus.Infrastructure.Data.Context;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Supplus.Infrastructure.Services;
+
+public class UsuarioAutenticadoService : IUsuarioAutenticado
+{
+    private readonly SupplusDbContext _dbContext;
+    private readonly ITokenProvider _tokenProvider;
+
+    public UsuarioAutenticadoService(SupplusDbContext dbContext, ITokenProvider tokenProvider)
+    {
+        _dbContext = dbContext;
+        _tokenProvider = tokenProvider;
+    }
+
+    public async Task<UsuarioAutenticado> ObterUsuarioAutenticadoAsync()
+    {
+        var token  = _tokenProvider.ObterToken();
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var jwtToken = tokenHandler.ReadJwtToken(token);
+
+        var identificador = jwtToken.Claims.First(c => c.Type == ClaimTypes.Sid).Value;
+        var identificadorUsuario = Guid.Parse(identificador);
+
+        return await _dbContext.Usuarios
+            .AsNoTracking()
+            .Where(u => u.IdExterno == identificadorUsuario)
+            .Select(u => new UsuarioAutenticado(u.Id, u.Email, u.Role))
+            .FirstAsync();
+    }
+}
