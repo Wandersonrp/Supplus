8147cf9579421b6f96775770a2b6f2dd553c4bd1
Wandersonrp <wandersonrp.49@hotmail.com>
2026-02-09 20:57:22 -0300

    adiciona teste de unidade de registrar usuario

---
+++ tests/Supplus.UseCases.Tests/Usuarios/Registrar/RegistrarUsuarioUseCaseTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Supplus.UseCases.Tests.Usuarios.Registrar;
+
+public class RegistrarUsuarioUseCaseTest
+{
+    [Fact]
+    public void Deve_Registrar_Usuario_Com_Sucesso()
+    {
+        // Arrange
+        var primeiroNome = "John";  
+        var sobrenome = "Doe";
+        var email = "johndoe@example.com";
+
+        var request = new RequestRegistrarUsuarioJson(primeiroNome, sobrenome, email);
+
+        // Act
+        var sut = new RegistrarUsuarioUseCase(request);
+
+        // Assert
+        Assert.True(sut.Sucesso);
+        Assert.NotNull(sut.Valor);
+        Assert.IsType<Guid>(sut.Valor.Id);
+    }
+}
