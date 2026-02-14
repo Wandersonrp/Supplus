966115e52e0a50f9f6b7c199e0b683434e0f66a3
Wanderson Rodrigues <91907486+Wandersonrp@users.noreply.github.com>
2026-02-14 09:17:01 -0300

    Feat/email service (#16)
    * adiciona código de erro 'SemPermissao'
    
    * adiciona ErroPadronizado SemPermissaoErro
    
    * adiciona case de SemPermissao para retornar o status codes devido
    
    * adiciona referẽncia de projeto
    
    * adiciona classe para leitura do appsettings e registrar serviço de e-mail no DI
    
    * adiciona serviço de envio de e-mail
    
    * adiciona EmailDTO para transporte de dados entre use cases e serviço de e-mail
    
    * adiciona user-secrets para usuário e senha das configurações do smtp do e-mail
    
    * adiciona classe para leitura das configurações de e-mail no appsettings
    
    * adiciona serviço de envio de e-mail no use case de registrar pessoa
    
    * adiciona summary para documentação
    
    * adiciona mock do serviço de e-mail
    
    * adiciona faker do serviço de e-mail para teste de integração
    
    * registra email faker
    
    * adiciona referência de projeto Application
    
    * adiciona email service no construtor do use case de registrar usuário
    
    * adiciona mensagem de erro de permissões inválidas
    
    * adiciona quebra de linha nos parâmetros do construtor
    
    * adiciona ProduceResponseType

--- src/Backend/Supplus.Api/Controllers/Usuarios/UsuariosController.cs
+++ src/Backend/Supplus.Api/Controllers/Usuarios/UsuariosController.cs
@@ -5,6 +5,7 @@ using Supplus.Api.Requirements;
 using Supplus.Application.UseCases.Usuarios.Perfil;
 using Supplus.Application.UseCases.Usuarios.Registrar;
 using Supplus.Comunicacao.Requests.Usuarios;
+using Supplus.Comunicacao.Responses;
 using Supplus.Comunicacao.Responses.Usuarios;
 
 namespace Supplus.Api.Controllers.Usuarios;
@@ -15,9 +16,9 @@ public class UsuariosController : BaseController
     [UsuarioAutenticado]
     [Authorize(Policies.PodeRegistrarUsuario)]
     [ProducesResponseType(typeof(ResponseUsuarioJson), StatusCodes.Status201Created)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status409Conflict)]
-    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ResponseErroJson), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ResponseErroJson), StatusCodes.Status409Conflict)]
+    [ProducesResponseType(typeof(ResponseErroJson), StatusCodes.Status403Forbidden)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public async Task<IActionResult> RegistrarUsuario(
         [FromBody] RequestRegistrarUsuarioJson request, 
@@ -35,6 +36,8 @@ public class UsuariosController : BaseController
     [HttpGet]
     [UsuarioAutenticado]
     [ProducesResponseType(typeof(ResponseUsuarioJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseErroJson), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public async Task<IActionResult> ObterPerfil([FromServices] IObterPerfilUseCase useCase, CancellationToken token)
     {
         var resultado = await useCase.Executar(token);
