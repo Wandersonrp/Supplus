50e04e61f75ebd7290dd5cc8337fd5e275567c97
Wandersonrp <wandersonrp.49@hotmail.com>
2026-02-13 22:35:12 -0300

    adiciona teste de unidade de obter perfil do usuário [red-green-refactor]

---
+++ tests/Supplus.UseCases.Tests/Usuarios/Perfil/ObterPerfilUseCaseTest.cs
@@ -0,0 +1,42 @@
+﻿using Supplus.Application.UseCases.Usuarios.Usuarios;
+using Supplus.Domain.Models;
+using UtilitariosCompartilhados.Tests.Builders.Services;
+
+namespace Supplus.UseCases.Tests.Usuarios.Perfil;
+
+public class ObterPerfilUseCaseTest
+{
+    [Fact(DisplayName = "Deve obter o perfil do usuário com sucesso.")]
+    public async Task Deve_Obter_Perfil_Com_Sucesso()
+    {
+        // Arrange
+
+        var usuarioAuenticado = new UsuarioAutenticado(
+            Id: 1,
+            Email: "admin@admin.com",
+            Role: Domain.Enums.Role.Administrador,
+            PrimeiroNome: "Admin",
+            Sobrenome: "Admin",
+            IdExterno: Guid.NewGuid());
+
+        var sut = CriarUseCase(usuarioAuenticado);
+
+        // Act
+        var resultado = await sut.Executar(CancellationToken.None);
+
+        // Assert
+        Assert.NotNull(resultado.Valor);
+        Assert.Equal(usuarioAuenticado.Email, resultado.Valor.Email);
+        Assert.IsType<Guid>(resultado.Valor.Id);
+    }
+
+    public static ObterPerfilUseCase CriarUseCase(UsuarioAutenticado? usuarioAutenticado = null)
+    {
+        var usuarioAutenticadoService = new UsuarioAutenticadoBuilder();
+
+        if (usuarioAutenticado is not null)
+            usuarioAutenticadoService.ObterUsuarioAutenticadoAsync(usuarioAutenticado);
+
+        return new ObterPerfilUseCase(usuarioAutenticadoService.Build());
+    }
+}
