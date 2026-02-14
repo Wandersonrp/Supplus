ad981a2b97d848097f169a89ec5be1cdb0d778d8
Wandersonrp <wandersonrp.49@hotmail.com>
2026-02-13 23:18:34 -0300

    adiciona método utilitário de usuário para realizar login

---
+++ tests/Supplus.Api.Tests/Utils/UsuarioUtils.cs
@@ -0,0 +1,15 @@
+﻿using Supplus.Comunicacao.Requests.Auth;
+using Supplus.Comunicacao.Responses.Auth;
+using System.Net.Http.Json;
+
+namespace Supplus.Api.Tests.Utils;
+
+public class UsuarioUtils
+{
+    public async Task<ResponseLoginJson?> AutenticarPorSenha(HttpClient httpClient, string rota, RequestLoginJson request)
+    {
+        var resposta = await httpClient.PostAsJsonAsync(rota, request);
+        resposta.EnsureSuccessStatusCode();
+        return await resposta.Content.ReadFromJsonAsync<ResponseLoginJson>();
+    }
+}
