a794d5c87c37b648ba08ac4fea1ba26e6fc2cddc
Wandersonrp <wandersonrp.49@hotmail.com>
2026-02-13 22:40:43 -0300

    adiciona use case de obter o perfil do usuário

---
+++ src/Backend/Supplus.Application/UseCases/Usuarios/Perfil/IObterPerfilUseCase.cs
@@ -0,0 +1,9 @@
+﻿using Supplus.Comunicacao.Responses.Usuarios;
+using Supplus.Exceptions;
+
+namespace Supplus.Application.UseCases.Usuarios.Perfil;
+
+public interface IObterPerfilUseCase
+{
+    Task<ResultadoPersonalizado<ResponseUsuarioJson>> Executar(CancellationToken token);
+}
