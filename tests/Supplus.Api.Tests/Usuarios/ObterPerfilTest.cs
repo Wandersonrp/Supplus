822207b6945a39c9bb3e0c11e218757ee3603093
Wandersonrp <wandersonrp.49@hotmail.com>
2026-02-13 23:19:51 -0300

    adiciona chamada do utilitário de usuário para realizar login

--- tests/Supplus.Api.Tests/Usuarios/ObterPerfilTest.cs
+++ tests/Supplus.Api.Tests/Usuarios/ObterPerfilTest.cs
@@ -1,4 +1,6 @@
-﻿using Supplus.Comunicacao.Requests.Auth;
+﻿using Supplus.Api.Tests.Utils;
+using Supplus.Comunicacao.Requests.Auth;
+using System.Net.Http.Headers;
 using System.Text.Json;
 
 namespace Supplus.Api.Tests.Usuarios;
@@ -10,11 +12,14 @@ public class ObterPerfilTest
 
     private readonly HttpClient _httpClient;
     private const string _rota = "/api/usuarios";
+    private UsuarioUtils _usuarioUtils;
+
 
     public ObterPerfilTest(ContainersFixture containersFixture)
     {
         _factory = new CustomWebApplicationFactory(containersFixture);
         _httpClient = _factory.CreateClient();
+        _usuarioUtils = new UsuarioUtils();
     }
 
     [Fact]
@@ -22,6 +27,13 @@ public class ObterPerfilTest
     {
         // Arrange
         var requestLogin = new RequestLoginJson(Email: "admin@admin.com", Senha: "SenhaForte123!");
+
+        // Realiza o login para obter o token de autenticação
+        var responseLogin = await _usuarioUtils.AutenticarPorSenha(_httpClient, rota: "/api/auth/login", requestLogin);
+
+        Assert.NotNull(responseLogin);
+
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", responseLogin.Token.AccessToken);
 
         // Act
         var resultado = await _httpClient.GetAsync(_rota);
@@ -33,7 +45,7 @@ public class ObterPerfilTest
 
         var id = jsonDocument.RootElement.GetProperty("id").GetGuid();
         var email = jsonDocument.RootElement.GetProperty("email").GetString();
-        var nomeCompleto = jsonDocument.RootElement.GetProperty("nome").GetString();
+        var nomeCompleto = jsonDocument.RootElement.GetProperty("nomeCompleto").GetString();
 
         // Assert
         Assert.IsType<Guid>(id);
