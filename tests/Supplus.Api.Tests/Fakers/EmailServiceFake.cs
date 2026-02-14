cf0d11fc6e55af5ec3aa0f565d9d8f0d5423b866
Wandersonrp <wandersonrp.49@hotmail.com>
2026-02-14 09:04:02 -0300

    adiciona faker do serviço de e-mail para teste de integração

---
+++ tests/Supplus.Api.Tests/Fakers/EmailServiceFake.cs
@@ -0,0 +1,9 @@
+﻿using Supplus.Application.DTOs;
+using Supplus.Application.Services.Notificacoes;
+
+namespace Supplus.Api.Tests.Fakers;
+
+public class EmailServiceFake : IEmailService
+{
+    public Task EnviarAsync(EmailDTO email) => Task.CompletedTask;
+}
