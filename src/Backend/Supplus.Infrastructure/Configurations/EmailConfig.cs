6bc98dfbe5e8f9a6dcc9a6117e6ad61b84d124dc
Wandersonrp <wandersonrp.49@hotmail.com>
2026-02-14 08:35:04 -0300

    adiciona classe para leitura das configurações de e-mail no appsettings

---
+++ src/Backend/Supplus.Infrastructure/Configurations/EmailConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace Supplus.Infrastructure.Configurations;
+
+/// <summary>
+/// Configurações necessárias para a conexão e autenticação com o servidor SMTP.
+/// Nota: 'Usuario' e 'Senha' devem ser configurados via User Secrets ou Environment Variables.
+/// </summary>
+public class EmailConfig
+{
+    public string SmtpServer { get; set; }
+    public int Port { get; set; }
+    public string SenderEmail { get; set; }
+    public bool EnableSsl { get; set; }
+    public string Usuario { get; set; }
+    public string Senha { get; set; }
+}
