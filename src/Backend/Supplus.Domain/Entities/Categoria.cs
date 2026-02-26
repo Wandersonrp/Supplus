91513a0c65f2acbdb19ecc604e2e00a1fa164a0c
Wandersonrp <wandersonrp.49@hotmail.com>
2026-02-25 23:10:48 -0300

    adiciona entidade Categoria

---
+++ src/Backend/Supplus.Domain/Entities/Categoria.cs
@@ -0,0 +1,18 @@
+﻿namespace Supplus.Domain.Entities;
+
+public sealed class Categoria : EntidadeBase
+{    
+    public string Nome { get; private set; }
+    public string Descricao { get; private set; }
+
+    private readonly List<Chamado> _chamados = new();
+    public IReadOnlyCollection<Chamado>  Chamados => _chamados.AsReadOnly();
+
+    private Categoria() { }
+
+    public Categoria(long criadoPor, string nome, string descricao) : base(criadoPor)
+    {        
+        Nome = nome;
+        Descricao = descricao;
+    }
+}
