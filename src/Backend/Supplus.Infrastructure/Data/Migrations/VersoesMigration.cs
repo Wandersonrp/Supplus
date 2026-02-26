0c1b0e1b5a0c5ae2bdcdb05a4959c682e1d12c7d
Wandersonrp <wandersonrp.49@hotmail.com>
2026-02-25 23:19:30 -0300

    adiciona tag de versões de migração do banco de dados

--- src/Backend/Supplus.Infrastructure/Data/Migrations/VersoesMigration.cs
+++ src/Backend/Supplus.Infrastructure/Data/Migrations/VersoesMigration.cs
@@ -4,4 +4,6 @@ public class VersoesMigration
 {
     public const int CRIAR_TABELA_USUARIO = 1;
     public const int CRIAR_TABELA_REFRESH_TOKEN = 2;
+    public const int CRIAR_TABELA_CATEGORIA = 3;
+    public const int CRIAR_TABELA_CHAMADO = 4;
 }
