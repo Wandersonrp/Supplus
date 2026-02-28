649015aaeb71a0d66d9a30b849d98172b30f78a4
Wandersonrp <wandersonrp.49@hotmail.com>
2026-02-27 23:04:28 -0300

    adiciona caso de uso de criar chamado

---
+++ src/Backend/Supplus.Application/UseCases/Chamados/Criar/CriarChamadoUseCase.cs
@@ -0,0 +1,62 @@
+﻿using Supplus.Comunicacao.Requests.Chamados;
+using Supplus.Comunicacao.Responses.Chamados;
+using Supplus.Domain.Entities;
+using Supplus.Domain.Enums;
+using Supplus.Domain.Repositories;
+using Supplus.Domain.Services;
+using Supplus.Exceptions;
+using Supplus.Exceptions.Mensagens;
+
+namespace Supplus.Application.UseCases.Chamados.Criar;
+
+public class CriarChamadoUseCase : ICriarChamadoUseCase
+{
+    private readonly IUsuarioAutenticado _usuarioAutenticadoService;
+    private readonly IRepository<Chamado> _chamadoRepository;
+    private readonly IRepository<Categoria> _categoriaRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CriarChamadoUseCase(
+        IUsuarioAutenticado usuarioAutenticadoService, 
+        IRepository<Chamado> chamadoRepository,
+        IRepository<Categoria> categoriaRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _usuarioAutenticadoService = usuarioAutenticadoService;
+        _chamadoRepository = chamadoRepository;
+        _categoriaRepository = categoriaRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<ResultadoPersonalizado<ResponseChamadoJson>> Executar(RequestCriarChamadoJson request, CancellationToken token)
+    {
+        var usuarioAutenticado = await _usuarioAutenticadoService.ObterUsuarioAutenticadoAsync(token);
+
+        var categoria = await _categoriaRepository.ObterPorIdExternoAsync(request.IdCategoria);
+
+        if (categoria is null)
+            return ResultadoPersonalizado<ResponseChamadoJson>
+                .Falha(ErroPadronizado.NaoEncontradoErro(String.Format(MensagensErro.NAO_ENCONTRADO, nameof(Categoria), request.IdCategoria)));
+
+        var chamado = new Chamado(
+            titulo: request.Titulo, 
+            descricao: request.Descricao, 
+            prioridade: (Prioridade)request.Prioridade, 
+            usuarioAutenticado.Id, 
+            idCategoria: categoria.Id);
+
+        await _chamadoRepository.AdicionarAsync(chamado);
+
+        await _unitOfWork.CommitAsync();
+
+        var response = new ResponseChamadoJson(
+            chamado.IdExterno, 
+            chamado.Titulo, 
+            chamado.Descricao, 
+            (Comunicacao.Enums.StatusChamado)chamado.StatusChamado, 
+            (Comunicacao.Enums.Prioridade) chamado.Prioridade, 
+            chamado.AbertoEm);
+
+        return ResultadoPersonalizado<ResponseChamadoJson>.Sucesso(response);
+    }
+}
