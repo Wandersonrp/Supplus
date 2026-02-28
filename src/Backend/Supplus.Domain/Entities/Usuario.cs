10a2737cc3e3b55389fd75dadb0846069a841fd5
Wandersonrp <wandersonrp.49@hotmail.com>
2026-02-28 19:46:49 -0300

    adiciona listas de chamados dos usuários

--- src/Backend/Supplus.Domain/Entities/Usuario.cs
+++ src/Backend/Supplus.Domain/Entities/Usuario.cs
@@ -15,9 +15,11 @@ public sealed class Usuario : EntidadeBase
     private readonly List<RefreshToken> _refreshTokens = new List<RefreshToken>();
     public IReadOnlyCollection<RefreshToken> RefreshTokens => _refreshTokens.AsReadOnly();
 
-    private readonly List<Chamado> _chamados = new List<Chamado>();
-    public IReadOnlyCollection<Chamado> Chamados => _chamados.AsReadOnly();
+    private readonly List<Chamado> _chamadosAgentes = new List<Chamado>();
+    public IReadOnlyCollection<Chamado> ChamadosAgentes => _chamadosAgentes.AsReadOnly();
 
+    private readonly List<Chamado> _chamadosUsuariosComuns = new List<Chamado>();
+    public IReadOnlyCollection<Chamado> ChamadosUsuariosComuns => _chamadosUsuariosComuns.AsReadOnly();
 
     private Usuario()
     {
