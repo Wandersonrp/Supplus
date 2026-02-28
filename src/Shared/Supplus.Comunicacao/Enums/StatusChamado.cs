f65cde9e91f829582c83e4262727c2b457824d5b
Wandersonrp <wandersonrp.49@hotmail.com>
2026-02-27 23:32:37 -0300

    adiciona request e response de chamado

---
+++ src/Shared/Supplus.Comunicacao/Enums/StatusChamado.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Supplus.Comunicacao.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum StatusChamado
+{
+    Aberto = 1,
+    EmAndamento = 2,
+    AguardandoCliente = 3,
+    Resolvido = 4,
+    Fechado = 5
+}
---
+++ src/Shared/Supplus.Comunicacao/Requests/Chamados/RequestCriarChamadoJson.cs
@@ -0,0 +1,9 @@
+﻿using Supplus.Comunicacao.Enums;
+
+namespace Supplus.Comunicacao.Requests.Chamados;
+
+public record RequestCriarChamadoJson(
+    string Titulo, 
+    string Descricao, 
+    Prioridade Prioridade,    
+    Guid IdCategoria);
